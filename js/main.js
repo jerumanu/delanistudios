92e92ff95e7a4d031252eb64ff5c4a252943647b
jerumanu <emmanueljeru200@gmail.com>
2020-10-08 22:38:51 +0300

    add $

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$ (document).ready( function(){
+    $("#image1").click(function () {
+    $("#para1").toggle(1000)
+    });
+    $("#image1").click(function () {
+        $("#para1").slidetoggle(1000);
+    });
+})
+$ (document).ready( function(){
+    $("#image2").click(function () {
+        $("#para2").toggle(1000)
+        
+    })
+    $("#image2").click(function () {
+        $("#para2").slidetoggle(1000);
+    });
+})
+$ (document).ready( function(){
+    $("#image3").click(function () {
+        $("#para3").toggle(1000)
+        
+    })
+    $("#image3").click(function () {
+        $("#para3").slidetoggle(1000);
+    });
+})
+
