76d530abdcc6e3730243c1da37f8d22eadccb686
jerumanu <emmanueljeru200@gmail.com>
2020-10-09 21:17:52 +0300

    make chanmges

--- js/main.js
+++ js/main.js
@@ -1,116 +1,107 @@
 $ (document).ready( function(){
-    $("#image1").click(function () {
-    $("#para1").slidetoggle(1000);
+    $("#image1a").click(function(){
+        $("#image1b").show(1000);
+        $("#image1a").hide(1000);
     });
-    $("#image1").click(function () {
-        $("#para1").slidetoggle(1000);
+    
+    $("#image1b").click(function(){
+        $("#image1a").show(1000);
+        $("#image1b").hide(1000);
     });
-})
-$ (document).ready( function(){
-    $("#image2").click(function () {
-        $("#para2").toggle(1000);
-        
-    })
-    $("#image2").click(function () {
-        $("#para2").slidetoggle(1000);
+    $("#image2a").click(function(){
+        $("#image2b").show(1000);
+        $("#image2a").hide(1000);
     });
-})
-$ (document).ready( function(){
-    $("#image3").click(function () {
-        $("#para3").toggle(1000);
-        
-    })
-    $("#image3").click(function () {
-        $("#para3").slidetoggle(1000);
+    $("#image2b").click(function(){
+        $("#image2a").show(1000);
+        $("#image2b").hide(1000);
     });
-})
-$(document).ready(function () {
+    $("#image3a").click(function(){
+        $("#image3b").show(1000);
+        $("#image3a").hide(1000);
+    });
+    $("#image3b").click(function(){
+        $("#image3a").show(1000);
+        $("#image3b").hide(1000);
+    });
    $ (".image1").mouseover(function () {
-       $("work1").show()
+       $("#work").show()
    });
 
      $ (".image1").mouseout(function () {
-       $("work1").hide();
+       $("#work").hide();
        
    })
-})
-$(document).ready(function () {
     $ (".image2").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show()
     });
  
       $ (".image2").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide();
         
     })
- })
+})
  $(document).ready(function () {
     $ (".image3").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image13").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
- $(document).ready(function () {
+ 
+ 
     $ (".image4").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image4").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
- $(document).ready(function () {
+ 
     $ (".image5").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image5").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
- $(document).ready(function () {
+ 
     $ (".image6").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image6").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
- $(document).ready(function () {
+ 
     $ (".image7").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image7").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
- $(document).ready(function () {
+
     $ (".image8").mouseover(function () {
-        $(".work").show(1000)
+        $("#work").show(1000)
     });
  
       $ (".image8").mouseout(function () {
-        $(".work").hide(1000);
+        $("#work").hide(1000);
         
     })
- })
-  $(document).ready(function () {
+ 
     
       $ ("#contact").submit(function ( )   {
           var Name =$("#name").value();
           var Email =$("#email").value();
         alert( " THANK YOU FOR CONTACTING US.WE HAVE GLADLY RECIEVED YOUR MESSAGE ");
         
-          
+      })   
       })
-  })
